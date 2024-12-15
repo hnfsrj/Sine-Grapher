11d6e7abc32ed6c904839522069526e557480b00
Hanif <hnfsrj@gmail.com>
2024-12-16 02:54:33 +0300

    refactor the structure and add bar graph

--- main.py
+++ main.py
@@ -3,143 +3,176 @@ import os
 import time
 
 
+space_char = "."
+point = "*"
+
+
+
+#translating the sine result to the y axis on our grid
+sine_to_grid = {
+    0.0:6, 
+    0.2:5, 
+    0.4:4, 
+    0.5:4, 
+    0.7:3, 
+    0.8:2, 
+    0.9:1, 
+    1:0,
+    -0.2:7, 
+    -0.4:8, 
+    -0.5:8, 
+    -0.7:9, 
+    -0.8:10, 
+    -0.9:11, 
+    -1:12
+}
+
+
+
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
 
-def dot_graph():
+def base_figure(width,height):
 
-    #width and height of the graph
-    width = 25
-    height = 13
-    space_char = "."
-    point = "*"
+    #initializing grid
+    grid = [[space_char for _ in range(width)] for _ in range(height)]
+
+    #inserting the 1,0, and -1 marks
+    grid[0][1] = "1"
+    grid[6][1] = "0"
+    grid[12][0] = "-"
+    grid[12][1] = "1"
+
+
+    #inserting the lines which mark the borders of the graph
+    for i in range(width-2):
+        grid[0][i+2] = "-"
+
+    for i in range(width-2):
+        grid[6][i+2] = "-"
+
+    for i in range(width-2):
+        grid[12][i+2] = "-"
+
+    for i in range(height):
+        if grid[i][1] == space_char:
+            grid[i][1] = "|"
+
+
+    return grid
+
+def plot_points_dot(num, grid, width, height):
+
+    for i in range(width-1):
+
+        x = ((i+num)*15)
+        sine_value = math.sin(math.radians(x))
+        truncated_value = int(sine_value*10)/10
+        y = sine_to_grid[truncated_value]
+
+        if y == 0:
+            grid[y][i+1] = "n"
+
+        elif y == height-1:
+            grid[y][i+1] = "u"
+
+        else:
+            grid[y][i+1] = point
+
+
+
+
+
+def plot_points_bar(num, grid, width, height):
+
+    for i in range(3):
+
+        x = ((i+num)*15)
+        sine_value = math.sin(math.radians(x))
+        truncated_value = int(sine_value*10)/10
+        y = sine_to_grid[truncated_value]
+
+
+        for j in range(3):
+            start = (i*6)+2
+
+            current_y = y
+            while(True):
+
+                grid[current_y][start+j] = point
+
+                if current_y > (height-1)/2:
+                    current_y-=1
+                elif current_y < (height-1)/2:
+                    current_y+=1
+                elif current_y == (height-1)/2:
+                    break
+
+
+
+
+
+def print_grid(grid):
+
+    #printing the grid to the console
+    for row in grid:
+        print("".join(row))
+
+
+
+
+
+
+
+
+
+
+def dot_graph(num, width, height):
+
 
     #generating the empty grid
-    grid = []
+    grid_dot = base_figure(width, height)
 
-    #translating the sine result to the y axis on our grid
-    sine_to_grid = {
-        0.0:6, 
-        0.2:5, 
-        0.4:4, 
-        0.5:4, 
-        0.7:3, 
-        0.8:2, 
-        0.9:1, 
-        1:0,
-        -0.2:7, 
-        -0.4:8, 
-        -0.5:8, 
-        -0.7:9, 
-        -0.8:10, 
-        -0.9:11, 
-        -1:12
-    }
-
-    def base_figure():
-        global grid
-
-        #initializing grid
-        grid = [[space_char for _ in range(width)] for _ in range(height)]
-
-        #inserting the 1,0, and -1 marks
-        grid[0][1] = "1"
-        grid[6][1] = "0"
-        grid[12][0] = "-"
-        grid[12][1] = "1"
-
-
-        #inserting the lines which mark the borders of the graph
-        for i in range(width-2):
-            grid[0][i+2] = "-"
-
-        for i in range(width-2):
-            grid[6][i+2] = "-"
-
-        for i in range(width-2):
-            grid[12][i+2] = "-"
-
-        for i in range(height):
-            if grid[i][1] == space_char:
-                grid[i][1] = "|"
-
-
-
-    def plot_points(num):
-        global grid
-        
-        for i in range(width-1):
-
-            x = ((i+num)*15)
-            sine_value = math.sin(math.radians(x))
-            truncated_value = int(sine_value*10)/10
-            y = sine_to_grid[truncated_value]
-
-            if y == 0:
-                grid[y][i+1] = "n"
-
-            elif y == height-1:
-                grid[y][i+1] = "u"
-
-            else:
-                grid[y][i+1] = point
+    plot_points_dot(num, grid_dot, width, height)
+    print_grid(grid_dot)
 
 
 
 
 
-    def print_grid():
-        global grid
-
-        #printing the grid to the console
-        for row in grid:
-            print("".join(row))
-
-
-    num = 0
-    while(True):
-        clear_console()
-
-        base_figure()
-        plot_points(num)
-        print_grid()
-
-        num+=1
-        time.sleep(1)
-
-
-
-
-def bar_graph():
-
-    while(True):
-        clear_console()
-        print("[]")
-
-        time.sleep(1)
-
-
-
-
-
-print("Sine Graph Plotter")
-
-while(True):
-
-    print("\nPress \'ctrl+c\' to exit once the plotting has started")
+def bar_graph(num, width, height):
     
-    print("\n1. Dot Graph\n2. Bar Graph\n3. Exit")
-    graph_type = int(input(": "))
+    #generating the empty grid
+    grid_bar = base_figure(width, height)
+    
+    plot_points_bar(num, grid_bar, width, height)
+    print_grid(grid_bar)
 
-    if graph_type == 1:
-        dot_graph();
-    elif graph_type == 2:
-        bar_graph();
-    elif graph_type == 3:
-        break
-    else:
-        print("\n invalid input!")
 
-print("exiting...")
+
+    
+
+#width and height of the graph
+width_dot = 25
+height_dot = 13
+
+width_bar = 17
+height_bar = 13
+
+    
+num = 0
+while(True):
+    clear_console()
+
+    print("Sine Graph Plotter")
+    print("\nPress \'ctrl+c\' to exit")
+
+
+    dot_graph(num, width_dot, height_dot)
+
+    bar_graph(num, width_bar, height_bar)
+
+    num+=1
+    time.sleep(1)
